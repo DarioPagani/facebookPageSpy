6933a15080ceebd0dc8e5f8d8e59f46edbac3287
Dario Pagani <dario.pagani.146@gmail.com>
2017-06-16 11:20:48 +0200

    Aggiunta classe Persona per selvaggio tracking

---
+++ js/Persona.js
@@ -0,0 +1,67 @@
+/**
+ * Migliarino Pisano, Vecchiano li 16/06/2017
+ * Classe per profilare le persone, ogni oggetto creato da questo oggetto
+ * è una persona di Facebook
+ * @atuhor: Dario Pagani <dario.pagani@itispisa.gov.it>
+ */
+ 
+class Persona
+{
+	constructor(id)
+	{
+		this.id = id;
+		this.like = new Array();
+		this.comments = new Array();
+	}
+}
+
+class Interazioni extends Array
+{
+	constructor()
+	{
+		super();
+	}
+
+	pushLike(persona, post, like)
+	{
+		var pos = super.findIndex(function(e,index)
+			{
+				return persona == e.id;
+			});
+		console.log(pos);
+		if(pos == -1)
+		{
+			var tmp = new Persona(persona);
+			tmp.like.push({post: post, like: like});
+			this.pushPersona(tmp);
+		}
+		else
+			this[pos].like.push({post: post, like: like});
+
+	}
+
+	pushComment(persona, post, comment)
+	{
+		var pos = super.findIndex(function(e,index)
+			{
+				return persona == e.id;
+			});
+		if(pos == -1)
+		{
+			var tmp = new Persona(persona);
+			tmp.comment.push({post: post, comment: comment});
+			this.pushPersona(tmp);
+		}
+		else
+			this[pos].comment.push({post: post, comment: comment});
+	}
+
+	pushPersona(persona)
+	{
+		super.push(persona);
+		super.sort(function(a,b)
+			{
+				return a.id - b.id;
+			});
+	}
+}
